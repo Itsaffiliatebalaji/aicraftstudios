aba69cc4e377ce632f6415a1e709fed6609a17c2
Itsaffiliatebalaji <balajigade534@gmail.com>
2026-06-07 17:46:10 +0530

    chnagess

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const morgan = require("morgan");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.use(helmet());
 app.use(cors());
